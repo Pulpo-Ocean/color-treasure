bd5e29ff3c04075836b2a43a7aaf4901a37b97e5
Pulpo-Ocean <otronivel123@gmail.com>
2026-09-08 20:31:22 +0200

    feat: add Unity special tile model

---
+++ unity/Assets/Scripts/Runtime/Gameplay/SpecialKind.cs
@@ -0,0 +1,12 @@
+namespace ColorTreasure.Runtime.Gameplay
+{
+    public enum SpecialKind
+    {
+        None = 0,
+        LineBurst = 1,
+        CrossBurst = 2,
+        AreaBomb = 3,
+        RainbowShell = 4,
+        TreasureBurst = 5
+    }
+}
